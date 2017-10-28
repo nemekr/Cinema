7a15ddc6539f774b2a6a9b84a773ff10ee92a81d
nemekr <krill.en@hotmail.com>
2017-10-28 12:50:12 +0200

    service kezdeti impl

---
+++ src/main/java/cinema/cinema/service/UserService.java
@@ -0,0 +1,107 @@
+package cinema.cinema.service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.annotation.SessionScope;
+
+import cinema.cinema.entity.Role;
+import cinema.cinema.entity.User;
+import cinema.cinema.repository.UserRepository;
+import lombok.Data;
+
+@Service
+@SessionScope
+@Data
+public class UserService {
+	@Autowired
+	private UserRepository userRepo;
+	
+	private ArrayList<User> loggedInUsers;
+	
+	private User user;
+	
+	public User login(String email, String password) {
+		Optional<User> daoUser = userRepo.findByEmail(email);
+		if (daoUser.isPresent()) {
+			user = daoUser.get();
+
+			if (user != null && (user.getPassword().equals(password))
+					&& (user.getEmail().equals(email))) {
+				if (loggedInUsers.contains(user)) {
+					throw new IllegalArgumentException("User already logged in!");
+				}
+				loggedInUsers.add(user);
+				return user;
+			}
+		}
+		throw new IllegalArgumentException("Wrong username or password!");
+
+	}
+
+
+	public void logout(User logOutUser) {
+		if (logOutUser != null && userRepo.findByEmail(logOutUser.getEmail()).isPresent()) {
+			if (loggedInUsers.contains(logOutUser)) {
+				loggedInUsers.remove(logOutUser);
+				user = null;
+			} else {
+				throw new IllegalArgumentException("User already logged out!");
+			}
+		}
+		throw new IllegalArgumentException("Invalid user!");
+	}
+
+	public User register(User registerUser) {
+		Optional<User> repoUser = userRepo.findByEmail(registerUser.getEmail());
+		if (repoUser.isPresent()) {
+			throw new IllegalArgumentException("User already exists!");
+		}
+
+		return userRepo.save(registerUser);
+	}
+	
+	@Transactional
+	public User registerAdmin(User promoteUser) {
+		if (promoteUser == null || promoteUser.getEmail() == null) {
+			throw new IllegalArgumentException("User cannot be null!");
+		}
+
+		if (!(userRepo.findByEmail(promoteUser.getEmail()).isPresent())) {
+			throw new IllegalArgumentException("User does not exists!");
+		}
+
+		promoteUser.setRole(Role.ADMIN);
+		return userRepo.save(promoteUser);
+	}
+
+	@Transactional
+	public User updateUser(User updatedUser) {
+		if (updatedUser == null || "".equals(updatedUser.getPassword()) || "".equals(updatedUser.getAddress())
+				|| "".equals(updatedUser.getName())) {
+			throw new IllegalArgumentException("One of the arguments are null!");
+		}
+
+		if (!(userRepo.findByEmail(updatedUser.getEmail()).isPresent())) {
+			throw new IllegalArgumentException("User does not exists!");
+		}
+
+		return userRepo.save(updatedUser);
+	}
+
+	@Transactional
+	public User deleteUser(User deleteUser) {
+		if (deleteUser == null) {
+			throw new NullPointerException("User cannot be null!");
+		} else if (!(userRepo.findByEmail(deleteUser.getEmail()).isPresent())) {
+			throw new IllegalArgumentException("User does not exists!");
+		}
+
+		userRepo.delete(deleteUser);
+		return deleteUser;
+	}
+}
